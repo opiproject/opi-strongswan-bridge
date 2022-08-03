9cd0996f68bcb33326b35f05822bfaac6111bfa6
Kyle Mestery <mestery@mestery.com>
2022-08-03 17:00:23 -0500

    security server: Config file and redis
    This commit adds Redis integration into the security PoC. We use Redis
    to store base64 encoded strings of protobuf definitions for now,
    allowing us to look them up after creation for get requests and such to
    return relevant data.
    
    This commit also adds some rudimentary strongSwan config file generation
    on the backend. Lots more to do here.
    
    Related #220
    
    The result is something like the following:
    
    ```
    2022/08/08 19:13:49 server listening at [::]:50151
    2022/08/08 19:13:49 IPsecCreate: Received: tunnels:{name:"Test Tunnel" local_ip:"192.168.200.200" remote_ip:"192.168.200.210" local_spi:3071981052 crypto_alg:AES_GCM_256 integ_alg:SHA_512_256}
    2022/08/08 19:13:49 Saving entry with UUID 7856db1e-3b2e-47ca-9f23-9136b6911171
    2022/08/08 19:13:49 Looking at file /etc/swanctl/TestTunnel.conf
    2022/08/08 19:13:49 ----- Config file -----
    2022/08/08 19:13:49
    TestTunnel {
        remote_addrs = 192.168.200.210
        vips = 0.0.0.0
        local {
            auth = pubkey
            certs = clientCert.pem
            id = client.strongswan.org
        }
        remote {
            auth = pubkey
            id = server.strongswan.org
        }
        children {
            net {
                remote_ts = 10.1.0.0/16
                esp_proposals = aes256gcm128-chacha20poly1305-x25519
                dpd_action = trap
            }
            host {
                esp_proposals = aes256gcm128-chacha20poly1305-x25519
                dpd_action = trap
            }
        }
        version = 2
        proposals = aes256-sha256-x25519
        dpd_delay = 60s
    }
    
    2022/08/08 19:13:49 -----------------------
    2022/08/08 19:13:49 Wrote 620 bytes to file
    2022/08/08 19:13:49 IPsecGet: Received: 7856db1e-3b2e-47ca-9f23-9136b6911171
    2022/08/08 19:13:49 IPsecUpdate: Received: 7856db1e-3b2e-47ca-9f23-9136b6911171
    2022/08/08 19:13:49 IPsecUpdate: Received: tunnels:{name:"Renamed Test Tunnel" local_ip:"192.168.200.220" remote_ip:"192.168.200.250" local_spi:4223652506 crypto_alg:AES_GMAC_256 integ_alg:SHA1_96 mode:TRANSPORT_MODE}
    2022/08/08 19:13:49 IPsecGet: Received: 7856db1e-3b2e-47ca-9f23-9136b6911171
    2022/08/08 19:13:49 IPsecDelete: Received: 7856db1e-3b2e-47ca-9f23-9136b6911171
    2022/08/08 19:13:49 Retreiving entry with UUID 7856db1e-3b2e-47ca-9f23-9136b6911171
    2022/08/08 19:13:49 Dumping unmarshaled protobuf
    tunnel:{tunnels:{name:"Test Tunnel" local_ip:"192.168.200.200" remote_ip:"192.168.200.210" local_spi:3071981052 crypto_alg:AES_GCM_256 integ_alg:SHA_512_256}}
    2022/08/08 19:13:49 Looking at file /etc/swanctl/TestTunnel.conf
    vagrant@bullseye:/git/opi-poc/security$
    ```
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- server/ipsec.go
+++ server/ipsec.go
@@ -5,15 +5,60 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"log"
 
+	"github.com/go-redis/redis/v8"
+	"google.golang.org/protobuf/proto"
 	pb "github.com/opiproject/opi-api/security/proto"
 	"github.com/google/uuid"
 )
 
+// Our client connection
+var rdb *redis.Client
+
+// Used for background Redis processing
+var ctx = context.Background()
+
+func init_backend() error {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	val, err := rdb.Info(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("Redis Info:\n%v\n", val)
+
+	return nil
+}
+
 func (s *server) IPsecCreate(ctx context.Context, in *pb.IPsecCreateRequest) (*pb.IPsecCreateResponse, error) {
 	log.Printf("IPsecCreate: Received: %v", in.GetTunnel())
 	ipsec_id := uuid.New()
+
+	// Store in Redis
+	b, err := proto.Marshal(in)
+	if err != nil {
+		log.Fatal("Cannot marshal data")
+	}
+	proto_str := base64.StdEncoding.EncodeToString(b)
+
+	log.Printf("Saving entry with UUID %s", ipsec_id.String())
+
+	err = rdb.Set(ctx, ipsec_id.String(), proto_str, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	err = load_connection("load", in)
+	if err != nil {
+		log.Printf("IPsecCreate: Failed with error %v", err)
+		return nil, err
+	}
 
 	ip_ret := pb.IPsecCreateResponse {
 		Id: &pb.Uuid {
@@ -25,7 +70,36 @@ func (s *server) IPsecCreate(ctx context.Context, in *pb.IPsecCreateRequest) (*p
 }
 
 func (s *server) IPsecDelete(ctx context.Context, in *pb.IPsecDeleteRequest) (*pb.IPsecDeleteResponse, error) {
-	log.Printf("IPsecDelete: Received: %v", in.GetId())
+	reqId := in.GetId().GetValue()
+	log.Printf("IPsecDelete: Received: %v", reqId)
+
+	log.Printf("Retreiving entry with UUID %s", reqId)
+
+	// Retreive from Redis
+	val, err := rdb.Get(ctx, reqId).Result()
+	if err != nil {
+		panic(err)
+	}
+	p_blob, err := base64.StdEncoding.DecodeString(val)
+	if err != nil {
+		log.Fatal("Cannot decode data")
+	}
+	ipsec_req := &pb.IPsecCreateRequest{}
+	err = proto.Unmarshal(p_blob, ipsec_req)
+	if err != nil {
+		log.Fatal("Cannot unmarshal data")
+	}
+
+	log.Printf("Dumping unmarshaled protobuf\n%v\n", ipsec_req)
+
+	err = delete_connection("load", ipsec_req)
+	if err != nil {
+		log.Printf("IPsecDelete: Failed with error %v", err)
+		return nil, err
+	}
+
+	// Delete from Redis
+	rdb.Del(ctx, reqId)
 
 	ip_ret := pb.IPsecDeleteResponse {
 		Id: &pb.Uuid {
@@ -36,7 +110,8 @@ func (s *server) IPsecDelete(ctx context.Context, in *pb.IPsecDeleteRequest) (*p
 }
 
 func (s *server) IPsecUpdate(ctx context.Context, in *pb.IPsecUpdateRequest) (*pb.IPsecUpdateResponse, error) {
-	log.Printf("IPsecUpdate: Received: %v", in.GetId())
+	reqId := in.GetId().GetValue()
+	log.Printf("IPsecUpdate: Received: %v", reqId)
 	log.Printf("IPsecUpdate: Received: %v", in.GetTunnel())
 
 	ip_ret := pb.IPsecUpdateResponse {
@@ -48,12 +123,14 @@ func (s *server) IPsecUpdate(ctx context.Context, in *pb.IPsecUpdateRequest) (*p
 }
 
 func (s *server) IPsecList(ctx context.Context, in *pb.IPsecListRequest) (*pb.IPsecListResponse, error) {
-	log.Printf("IPsecList: Received: %v", in.GetId())
+	reqId := in.GetId().GetValue()
+	log.Printf("IPsecList: Received: %v", reqId)
 	return &pb.IPsecListResponse{}, nil
 }
 
 func (s *server) IPsecGet(ctx context.Context, in *pb.IPsecGetRequest) (*pb.IPsecGetResponse, error) {
-	log.Printf("IPsecGet: Received: %v", in.GetId())
+	reqId := in.GetId().GetValue()
+	log.Printf("IPsecGet: Received: %v", reqId)
 
 	ip_ret := pb.IPsecGetResponse {
 		Id: &pb.Uuid {
