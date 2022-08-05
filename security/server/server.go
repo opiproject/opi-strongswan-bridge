a88035c3146d2e4a7adb2d80a6d0d656b4f81460
Boris Glimcher <Boris.Glimcher@emc.com>
2022-08-05 17:20:51 +0300

    storage: security: add reflection
    From https://github.com/grpc/grpc-go/blob/master/Documentation/server-reflection-tutorial.md
    
    Example:
    
    $ docker run --network=storage_opi --rm -it namely/grpc-cli ls 192.168.208.2:50051
    grpc.reflection.v1alpha.ServerReflection
    opi.storage.v1.NVMeControllerService
    opi.storage.v1.NVMeNamespaceService
    opi.storage.v1.NVMeSubsystemService
    opi.storage.v1.NVMfRemoteControllerService
    opi.storage.v1.VirtioBlkService
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- security/server/server.go
+++ security/server/server.go
@@ -11,6 +11,7 @@ import (
 
 	"google.golang.org/grpc"
 	pb "github.com/opiproject/opi-api/security/proto"
+	"google.golang.org/grpc/reflection"
 )
 
 var (
@@ -30,6 +31,8 @@ func main() {
 	s := grpc.NewServer()
 
 	pb.RegisterIPsecServer(s, &server{})
+
+	reflection.Register(s)
 
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
