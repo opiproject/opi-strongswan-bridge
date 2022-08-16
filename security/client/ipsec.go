c0065c144d6fa9eb31999d190dfa6555c45ac935
Kyle Mestery <mestery@mestery.com>
2022-08-16 14:45:55 -0500

    security: Move to new security APIs
    A recent commit [1] into the `opi-api` repository revamped the security
    APIs. This commit in the `opi-poc` repository migrates the client and
    server code to using the new APIs. The current status of the work is
    such that we can load and unload connections into strongSwan. Subsequent
    work will handle initiating and terminating connections, and flesh out
    parameter handling.
    
    Related to #220
    
    [1] https://github.com/opiproject/opi-api/pull/94
    
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- security/client/ipsec.go
+++ security/client/ipsec.go
@@ -6,7 +6,6 @@ package main
 import (
 	"context"
 	"log"
-	"math/rand"
 
 	pb "github.com/opiproject/opi-api/security/proto"
 	"google.golang.org/grpc"
@@ -16,85 +15,54 @@ func do_ipsec(conn grpc.ClientConnInterface, ctx context.Context) {
 	// IPsec
 	c1 := pb.NewIPsecClient(conn)
 
-	// Create IPsec Connection
-	local_ipsec := pb.IPsecCreateRequest{
+	// Load IPsec connection
+	local_ipsec := pb.IPsecLoadConnReq {
+		Connection: &pb.Connection {
+			Name: "opi-test",
+			Version: "2",
+			LocalAddrs: []*pb.Addrs {
+				{
+					Addr: "192.168.200.200",
+				},
+			},
+			RemoteAddrs: []*pb.Addrs {
+				{
+					Addr: "192.168.200.210",
+				},
+			},
+			Proposals: &pb.Proposals {
+					CryptoAlg: []pb.CryptoAlgorithm { pb.CryptoAlgorithm_AES256GCM128 },
+					IntegAlg: []pb.IntegAlgorithm { pb.IntegAlgorithm_SHA256_96 },
+					Dhgroups: []pb.DiffieHellmanGroups { pb.DiffieHellmanGroups_CURVE25519 },
+			},
+			Children: []*pb.Child {
+				{
+					Name: "opi-child",
+					EspProposals: &pb.Proposals {
+							CryptoAlg: []pb.CryptoAlgorithm { pb.CryptoAlgorithm_AES256GMAC },
+							IntegAlg: []pb.IntegAlgorithm { pb.IntegAlgorithm_SHA512 },
+							Dhgroups: []pb.DiffieHellmanGroups { pb.DiffieHellmanGroups_CURVE25519 },
+					},
+				},
+			},
+		},
+	}
+
+	rs1, err := c1.IPsecLoadConn(ctx, &local_ipsec)
+	if err != nil {
+		log.Fatalf("could not load IPsec tunnel: %v", err)
+	}
+	log.Printf("Loaded: %v", rs1)
+
+	// Unload
+
+	unload_ipsec := pb.IPsecUnloadConnReq {
 		Name: "opi-test",
-		Tunnel: &pb.TunnelInterfaces{
-			Tunnels: []*pb.TunnelInterfaces_Tunnel{
-				{
-					Name: "Test Tunnel",
-					LocalIp: "192.168.200.200",
-					RemoteIp: "192.168.200.210",
-					LocalSpi: rand.Uint32(),
-					CryptoAlg: pb.CryptoAlgorithm_AES256GCM128,
-					IntegAlg: pb.IntegAlgorithm_SHA256_96,
-					Dhgroups: pb.DiffieHellmanGroups_CURVE25519,
-					Mode: pb.IPsecMode_TUNNEL_MODE,
-				},
-			},
-		},
-		Sa: &pb.SecurityAssociations {
-			Sas: []*pb.SecurityAssociations_Sa {
-				{
-					Name: "Security Association One",
-					Spi: 8000,
-					Protocol: pb.SecurityAssociations_Sa_ESP,
-					CryptoAlg: pb.CryptoAlgorithm_AES256GMAC,
-					IntegAlg: pb.IntegAlgorithm_SHA512,
-					Dhgroups: pb.DiffieHellmanGroups_CURVE25519,
-				},
-			},
-		},
 	}
 
-	rs1, err := c1.IPsecCreate(ctx, &local_ipsec)
+	rs2, err := c1.IPsecUnloadConn(ctx, &unload_ipsec)
 	if err != nil {
-		log.Fatalf("could not create IPsec tunnel: %v", err)
+		log.Fatalf("could not unload IPsec tunnel: %v", err)
 	}
-	log.Printf("Added: %v", rs1)
-
-	// Get the IPsec Connection
-	rs2, err := c1.IPsecGet(ctx, &pb.IPsecGetRequest { Id: rs1.GetId() })
-	if err != nil {
-		log.Fatalf("could not get IPsec tunnel: %v", err)
-	}
-	log.Printf("Get: %v", rs2)
-
-	// Update the IPsec Connection
-	update_ipsec := pb.IPsecUpdateRequest {
-		Id: rs1.GetId(),
-		Tunnel: &pb.TunnelInterfaces {
-			Tunnels: []*pb.TunnelInterfaces_Tunnel {
-				{
-						Name: "Renamed Test Tunnel",
-						LocalIp: "192.168.200.220",
-						RemoteIp: "192.168.200.250",
-						LocalSpi: rand.Uint32(),
-						CryptoAlg: pb.CryptoAlgorithm_AES256,
-						IntegAlg: pb.IntegAlgorithm_MD5_128,
-						Mode: pb.IPsecMode_TRANSPORT_MODE,
-				},
-			},
-		},
-	}
-
-	rs3, err := c1.IPsecUpdate(ctx, &update_ipsec)
-	if err != nil {
-		log.Fatalf("could not update IPsec tunnel: %v", err)
-	}
-	log.Printf("Updated: %v", rs3)
-
-	// Get the IPsec Connection
-	rs4, err := c1.IPsecGet(ctx, &pb.IPsecGetRequest { Id: rs1.GetId() })
-	if err != nil {
-		log.Fatalf("could not get IPsec tunnel: %v", err)
-	}
-	log.Printf("Get: %v", rs4)
-
-	// Delete IPsec Connection
-	rs5, err := c1.IPsecDelete(ctx, &pb.IPsecDeleteRequest { Id: rs1.GetId() })
-	if err != nil {
-		log.Fatalf("could not delete IPsec tunnel: %v", err)
-	}
-	log.Printf("Deleted: %v", rs5)
+	log.Printf("Unloaded: %v", rs2)
 }
--- security/server/go.mod
+++ security/server/go.mod
@@ -3,20 +3,16 @@ module main
 go 1.18
 
 require (
-	github.com/go-redis/redis/v8 v8.11.5
-	github.com/google/uuid v1.3.0
-	github.com/opiproject/opi-api v0.0.0-20220811205735-2cdd70e3c937
+	github.com/opiproject/opi-api v0.0.0-20220817144324-1508f5ff11d5
 	github.com/strongswan/govici v0.6.0
 	google.golang.org/grpc v1.48.0
-	google.golang.org/protobuf v1.28.1
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	golang.org/x/net v0.0.0-20220805013720-a33c5aa5df48 // indirect
 	golang.org/x/sys v0.0.0-20220804214406-8e32c043e418 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220805133916-01dd62135a58 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
